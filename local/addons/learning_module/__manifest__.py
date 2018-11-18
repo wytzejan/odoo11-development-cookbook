56d9765926b66b86f1e33f6e3df4946b6828641e
wytzejan <10392890+wytzejan@users.noreply.github.com>
2018-11-18 19:47:09 +0100

    Development Cookbook chapter 4 until page 105

---
+++ local/addons/learning_module/__manifest__.py
@@ -0,0 +1,13 @@
+{
+ 'name': "Learning module",
+ 'summary': "Short subtitle phrase",
+ 'description': """Long description""",
+ 'author': "codeNext",
+ 'license': "AGPL-3",
+ 'website': "http://www.codenext.nl",
+ 'category': 'Uncategorized',
+ 'version': '11.0.1.0.0',
+ 'depends': ['base'],
+ #'data': ['views.xml'],
+ #'demo': ['demo.xml'],
+}
