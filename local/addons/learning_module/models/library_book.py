5f5d97a8588df2364e200d953031e367120e2371
wytzejan <10392890+wytzejan@users.noreply.github.com>
2018-11-27 21:06:47 +0100

    Development Cookbook chapter 5 until page 116

--- local/addons/learning_module/models/library_book.py
+++ local/addons/learning_module/models/library_book.py
@@ -2,7 +2,11 @@ from odoo import models, fields
 
 class LibraryBook(models.Model):
     _name = 'library.book'
+    _description = 'Library Book'
+    _order = 'date_release desc, name'
+    _rec_name = 'short_name'
     name = fields.Char('Title', required=True)
+    short_name = fields.Char('Short Title', required=True)
     date_release = fields.Date('Release Date')
     author_ids = fields.Many2many(
         'res.partner',
