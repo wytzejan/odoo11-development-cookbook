b848f87468a02d6af7ac228a74f3f7da05676ff7
wytzejan <10392890+wytzejan@users.noreply.github.com>
2018-12-28 15:13:48 +0100

    Development Cookbook chapter 5 until page 145

--- local/addons/learning_module/models/library_book.py
+++ local/addons/learning_module/models/library_book.py
@@ -1,5 +1,8 @@
-from odoo import models, fields
+from odoo import models, fields, api
 from odoo.addons import decimal_precision as dp
+from odoo.fields import Date as fDate
+from datetime import timedelta
+
 
 class LibraryBook(models.Model):
     _name = 'library.book'
@@ -10,8 +13,8 @@ class LibraryBook(models.Model):
     short_name = fields.Char(
         string='Short Title',
         required=True,
-        size=100, # For Char only
-        translate=False, # also for Text fields
+        size=100,  # For Char only
+        translate=False,  # also for Text fields
     )
     date_release = fields.Date('Release Date')
     author_ids = fields.Many2many(
@@ -48,7 +51,7 @@ class LibraryBook(models.Model):
     )
     reader_rating = fields.Float(
         'Reader Average Rating',
-        digits=(14, 4), # Optional precision (total, decimals),
+        digits=(14, 4),  # Optional precision (total, decimals),
     )
     cost_price = fields.Float(
         'Book Cost', dp.get_precision('Book Price'))
@@ -65,9 +68,72 @@ class LibraryBook(models.Model):
         context={},
         domain=[],
     )
+    publisher_city = fields.Char(
+        'Publisher City',
+        related='publisher_id.city',
+        readonly=True)
+    age_days = fields.Float(
+        string='Days Since Release',
+        compute='_compute_age',
+        inverse='_inverse_age',
+        search='_search_age',
+        store=False,
+        compute_sudo=False,
+    )
+    ref_doc_id = fields.Reference(
+        selection='_referencable_models',
+        string='Reference Document')
+
+    _sql_constraints = [
+        ('name_uniq',
+         'UNIQUE (name)',
+         'Book title must be unique.')
+    ]
+
+    @api.constrains('date_release')
+    def _check_release_date(self):
+        for record in self:
+            if (record.date_release and
+                    record.date_release > fields.Date.today()):
+                raise models.ValidationError(
+                    'Release date must be in the past')
+
+    @api.depends('date_release')
+    def _compute_age(self):
+        today = fDate.from_string(fDate.today())
+        for book in self.filtered('date_release'):
+            delta = (today -
+                     fDate.from_string(book.date_release))
+            book.age_days = delta.days
+
+    def _inverse_age(self):
+        today = fDate.from_string(fDate.context_today(self))
+        for book in self.filtered('date_release'):
+            d = today - timedelta(days=book.age_days)
+            book.date_release = fDate.to_string(d)
+
+    def _search_age(self, operator, value):
+        today = fDate.from_string(fDate.context_today(self))
+        value_days = timedelta(days=value)
+        value_date = fDate.to_string(today - value_days)
+        # convert the operator
+        # book with age > value have a date < value_date
+        operator_map = {
+            '>': '<', '>=': '<=',
+            '<': '>', '<=': '>=',
+        }
+        new_op = operator_map.get(operator, operator)
+        return [('date_release', new_op, value_date)]
+
+    @api.model
+    def _referencable_models(self):
+        models = self.env['res.request.link'].search([])
+        return [(x.object, x.name) for x in models]
+
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
+    _order = 'name'
     published_book_ids = fields.One2many(
         'library.book', 'publisher_id',
         string='Published Books')
@@ -76,3 +142,11 @@ class ResPartner(models.Model):
         string='Authored Books',
         # relation='library_book_res_partner_rel' # optional
     )
+    count_books = fields.Integer(
+        'Number of Authored Books',
+        compute='_compute_count_books')
+
+    @api.depends('authored_book_ids')
+    def _compute_count_books(self):
+        for r in self:
+            r.count_books = len(r.authored_book_ids)
