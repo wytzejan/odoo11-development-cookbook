031f0b1b73ae7ebf2bcb385b4c6b41193c1384f8
wytzejan <10392890+wytzejan@users.noreply.github.com>
2019-01-12 20:31:22 +0100

    Development Cookbook chapter 6 until page 183

--- local/addons/learning_module/models/library_book.py
+++ local/addons/learning_module/models/library_book.py
@@ -12,6 +12,7 @@ class BaseArchive(models.AbstractModel):
         for record in self:
             record.active = not record.active
 
+
 class LibraryBook(models.Model):
     _name = 'library.book'
     _inherit = ['base.archive']
@@ -187,18 +188,41 @@ class LibraryBook(models.Model):
                 )
         return super(LibraryBook, self).write(values)
 
-    @api.model
-    def fields_get(self,
-                   allfields=None,
-                   attributes=None):
-        fields = super(LibraryBook, self).fields_get(
-            allfields=allfields,
-            attributes=attributes
-        )
-        if not self.user_has_groups(
-            'library.group_library_manager'):
-            if 'manager_remarks' in fields:
-                fields['manager_remarks']['readonly'] = True
+    # @api.model
+    # def fields_get(self,
+    #                allfields=None,
+    #                attributes=None):
+    #     fields = super(LibraryBook, self).fields_get(
+    #         allfields=allfields,
+    #         attributes=attributes
+    #     )
+    #     if not self.user_has_groups(
+    #         'library.group_library_manager'):
+    #         if 'manager_remarks' in fields:
+    #             fields['manager_remarks']['readonly'] = True
+
+    @api.multi
+    def name_get(self):
+        result = []
+        for book in self:
+            authors = book.author_ids.mapped('name')
+            name = '%s (%s)' % (book.name, ', '.join(authors))
+            result.append((book.id, name))
+        return result
+
+    @api.multi
+    def _name_search(self, name='', args=None, operator='ilike',
+                     limit=100, name_get_uid=None):
+        args = [] if args is None else args.copy()
+        if not(name == '' and operator == 'ilike'):
+            args += ['|', '|',
+                     ('name', operator, name),
+                     ('isbn', operator, name),
+                     ('author_ids.name', operator, name)]
+        return super(LibraryBook, self)._name_search(
+            name='', args=args, operator='ilike',
+            limit=limit, name_get_uid=name_get_uid)
+
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
@@ -219,6 +243,7 @@ class ResPartner(models.Model):
     def _compute_count_books(self):
         for r in self:
             r.count_books = len(r.authored_book_ids)
+
 
 class LibraryMember(models.Model):
     _name = 'library.member'
